70bf21e127d963d4f4f6fc26877d17838507ba7b
Daniel Yoachim <dyoachim@gmail.com>
2014-08-08 20:20:39 -0500

    add block opponent method

--- elTicTac.rb
+++ elTicTac.rb
@@ -1,6 +1,10 @@
 class TicTacToe
 
   def game_session
+    
+    system 'clear'
+
+    round = 1
     @board = ["_", "_", "_",
               "_", "_", "_",
               "_", "_", "_",
@@ -11,8 +15,11 @@ class TicTacToe
       player_turn("X")
       break if check_game("X")
 
-      player_turn("O")
+      #player_turn("O")
+      computer_turn(round)
       break if check_game("O")
+
+      round += 1
     end
     
     display_board
@@ -25,6 +32,7 @@ class TicTacToe
   private
 
   def display_board
+    system 'clear'
     board = @board
     puts ".........."
     puts board[0] + board[1] + board[2]
@@ -39,10 +47,39 @@ class TicTacToe
     move = gets.chomp.to_i - 1
     if @board[move] != '_'
       puts "Already taken!"
+      sleep(1)
       player_turn(player)
       return
     end
     @board[move] = player 
+  end
+
+  def computer_turn(round)
+    display_board
+    puts "O:"
+    sleep(1)
+
+    return if try_blocking
+
+    if round == 1
+      if [@board[0],@board[2],@board,[6],@board[8]].include?('X') && @board[4] == '_'
+        move = 4
+
+      elsif @board[4] == 'X'
+          move = 0 unless @board[0] != '_'
+          move = 2 unless @board[2] != '_'
+          move = 6 unless @board[6] != '_'
+          move = 8 unless @board[8] != '_'
+           
+      elsif [@board[1],@board[3],@board,[5],@board[7]].include?('X')
+      end
+    end
+
+    if move 
+      @board[move] = "O" 
+    else 
+      puts 'Panic'
+    end
   end
 
   def check_game(player)
@@ -59,6 +96,31 @@ class TicTacToe
       #diagonal
       board[0] + board[4] + board[8],
       board[2] + board[4] + board[6] ].include?(player*3)
+  end
+
+  def try_blocking
+    board = @board
+    move = false
+    block_chance = ['_XX','X_X','XX_']
+
+
+    [0,3,6].each do |i|
+      line = [board[i], board[i+1], board[i+2]].join
+      block_chance.include?(line) ? move = i + line.index('_') : false
+    end
+
+    [0,1,2].each do |i|
+      line = [board[i], board[i+3], board[i+6]].join
+      block_chance.include?(line) ? move = i + line.index('_') * 3 : false
+    end
+
+    diagonal_backward = [board[0], board[4], board[8]].join
+    diagional_forward = [board[2], board[4], board[6]].join
+
+    block_chance.include?(diagonal_backward) ? move = diagonal_backward.index('_') * 4     : false
+    block_chance.include?(diagional_forward) ? move = diagional_forward.index('_') * 2 + 2 : false
+
+    move ? @board[move] = "O" : false
   end
 end
 
