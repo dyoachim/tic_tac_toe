28e0ebdb39f099c2cc1493d919645762b93d5098
Daniel Yoachim <dyoachim@gmail.com>
2014-08-18 11:26:59 -0500

    sanitized input for human players

--- player.rb
+++ player.rb
@@ -9,12 +9,16 @@ class Player
   	@board.display_board
     puts @letter + ":"
     move = gets.chomp.to_i - 1
-
-    if @board.board[move] != '_'
-      puts "Already taken!"
+    
+    if !((0..8).include?(move))
+      puts "Invalid move!"
       sleep(1)
       self.take_turn
       return
+    elsif @board.board[move] != '_'
+      puts "Already taken!"
+      sleep(1)
+      self.take_turn
     end
     
     @board.board[move] = @letter
