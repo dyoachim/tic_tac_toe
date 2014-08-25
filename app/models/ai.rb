67349c421a9f3946d5c866027eabf29a932bbc27
Daniel Yoachim <dyoachim@gmail.com>
2014-08-25 13:31:22 -0500

    simplified ai#make_move responsibility

--- app/models/ai.rb
+++ app/models/ai.rb
@@ -21,37 +21,22 @@ class Tic_tac_AI
   def make_move
     win_chance = [@letter, @letter,'_','_','_'].permutation(3).map(&:join).uniq
     board = @board.board
+    choice = []
 
-    if board.include?(@opponent_letter) && !(board.include?(@letter))
-      if @board.open_space?(4)
-        choice = [4]
-      else
-        pick = [0,2,6,8].sample until pick && @board.open_space?(pick)
-        choice = [pick]
+    @board.combos.each do |win_numbers|
+      if board.uniq.length == 1 || board.uniq.length == 2
+        choice << 4
+        [0,2,6,8].shuffle.each { |place| choice << place }
       end
-    elsif win_chance.include?([board[3], board[4], board[5]].join)
-      choice = [3,5]
-    elsif win_chance.include?([board[1], board[4], board[7]].join)
-      choice = [1,7]
-    elsif win_chance.include?([board[0], board[3], board[6]].join)
-      choice = [0,6]
-    elsif win_chance.include?([board[2], board[5], board[8]].join)
-      choice = [2,8]
-    elsif win_chance.include?([board[0], board[1], board[2]].join)
-      choice = [0,2]
-    elsif win_chance.include?([board[6], board[7], board[8]].join)
-      choice = [6,8]
-    elsif win_chance.include?([board[2], board[4], board[6]].join)
-      choice = [2,6]
-    else 
-      choice = [board.index('_')]
+
+      if win_chance.include?([ board[win_numbers[0]], board[win_numbers[1]], board[win_numbers[2]]].join)
+        choice << win_numbers[0]
+        choice << win_numbers[2]
+      end
     end
 
-    if @board.open_space?(choice[0])
-      choice[0]
-    else
-      choice[1]
-    end
+    choice << board.index('_')
+    choice.uniq.find {|place| @board.open_space?(place) }
   end
 
   def can_win?
