6bc6174eab41b33f09d8633a3498e888ab8aaa3e
Daniel Yoachim <dyoachim@gmail.com>
2014-08-25 17:41:39 -0500

    tab formatting fixes

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,15 +3,15 @@ require "./app/models/board.rb"
 
 describe Player do
 
-	let!(:board) { Board.new }
-	let(:player) { Player.new(board, "X")}
+  let!(:board) { Board.new }
+  let(:player) { Player.new(board, "X")}
 
 	
-	it 'has a letter' do
+  it 'has a letter' do
     expect(player.letter).to eq("X")
   end
 
   it 'responds to #take_turn' do
-		expect(player).to respond_to(:take_turn)
-	end
+    expect(player).to respond_to(:take_turn)
+  end
 end
